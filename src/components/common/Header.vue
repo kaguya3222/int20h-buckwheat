fd9f8a1989d53437798a69034acc2cb99c14df83
Andrew <andrewkarp00@gmail.com>
2021-01-21 18:36:30 +0200

    feat: created Header component

---
+++ src/components/common/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app-bar app color="brown darken-2" dark>
+    <v-app-bar-title>int20h buckwheat</v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
