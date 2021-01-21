e12d56c7399156fe06312abd0c384b09d444b634
Andrew <andrewkarp00@gmail.com>
2021-01-21 18:48:47 +0200

    feat: made up application layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
-  <router-view />
+  <v-app>
+    <Header />
+    <v-main>
+      <v-container fluid>
+        <router-view />
+      </v-container>
+    </v-main>
+    <Footer />
+  </v-app>
 </template>
 
 <script>
+import Footer from '@/components/common/Footer'
+import Header from '@/components/common/Header'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  }
 }
 </script>
