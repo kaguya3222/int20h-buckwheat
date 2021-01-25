58ceeeb4b05a948c2821f4b00fcd35d0ee6a6ba8
Andrew <andrewkarp00@gmail.com>
2021-01-25 23:08:24 +0200

    feat: added navigation

---
+++ src/components/common/Navbar/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-btn v-for="link in links" :key="link.name" :to="link.to" text>{{
+      link.name
+    }}</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+      links: [{ name: 'Графік цін', to: { name: 'chart' } }]
+    }
+  }
+}
+</script>
